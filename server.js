2da0041c103bd090b1e30f6c84a672f5e9edde28
Vogeln7 <david.vogel.7@gmail.com>
2021-07-21 22:35:58 -0300

    Se modifica las vistas y el server para utilizar OUG como motor de plantillas
    Signed-off-by: Vogeln7 <david.vogel.7@gmail.com>

---
+++ server.js
@@ -0,0 +1,64 @@
+//IMPORTACION
+const express=require('express');
+let producto=require('./productos');
+const pug=require('pug');
+
+//INSTANCIAS
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static('./public'));
+app.set('views','./views')
+app.set('view engine','pug')
+
+const routerApp=express.Router();
+const PORT=8080;
+
+routerApp.get('/productos',(req,res)=>{
+    res.send(producto.productosListados)
+})
+routerApp.get('/',(req,res)=>{
+    res.render('index')
+})
+routerApp.get('/productos/vista',(req,res)=>{
+    let arrayProductos=producto.productosListados;
+    
+    if(arrayProductos.error){
+        res.render('error',{error:arrayProductos.error})
+    }else{
+        res.render('productos',{productos:arrayProductos})
+    }
+    
+})
+
+routerApp.get('/productos/:id',(req,res)=>{
+    let idProducto=req.params.id;    
+    res.send(producto.filtrarPorId(idProducto))
+})
+
+routerApp.post('/productos/guardar',(req,res)=>{
+    let productoAGuardar=req.body;           
+    producto.nuevoProducto(productoAGuardar);        
+    res.redirect('/api');
+})
+
+routerApp.put('/productos/actualizar/:id',(req,res)=>{
+    let productoAActualizar=req.body;   
+    let idProductoAActualizar=req.params.id;
+    let productoActualizado=producto.actualizarPorId(idProductoAActualizar,productoAActualizar);
+    res.send(productoActualizado);
+})
+
+routerApp.delete('/productos/borrar/:id',(req,res)=>{
+    let idProductoABorrar=req.params.id;   
+    let productoBorrado=producto.borrarPorId(idProductoABorrar);
+    res.send(productoBorrado);
+})
+
+app.use('/api',routerApp)
+
+app.listen(PORT,()=>{
+    console.log(`El servidor se creó correctamente y esta escuchando peticiones en el puerto: ${PORT}`)
+})
